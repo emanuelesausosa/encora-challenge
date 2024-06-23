cf3f2c087474558a73e25c42a35bafa6d165764e
Emanuel Sosa <emanuelesausosa@gmail.com>
2024-06-23 16:42:49 -0600

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Use api (Emanuel Sosa and Encora challenge)')
+    .setDescription('This is a simple user api for the Encora challenge')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/models/dtos/UserDto.ts
+++ src/user/models/dtos/UserDto.ts
@@ -8,8 +8,6 @@ import {
 } from 'class-validator';
 
 // User dto is a class that is used to transfer data between the client and the server.
-// It is a simple class with properties that represent the data that is being transferred.
-// For example, you can have a password property in the entity but not in the dto.
 export class UserDto {
   @IsString()
   @IsOptional()
